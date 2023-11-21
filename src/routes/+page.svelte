610b2bf81dae5f6c887f9a80f3afc072714f63ae
Kip <Kipling.Dunlap@gmail.com>
2023-11-21 16:47:44 -0500

    Initial upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+  import '@arcgis/core/assets/esri/themes/dark/main.css';
+  import { defineCustomElements } from '@arcgis/map-components/dist/loader';
+  defineCustomElements();
+</script>
+
+<!-- create a heatmap from the BuildingPermits layer -->
+
+<arcgis-identity />
+<arcgis-map item-id="2876983423be46de943ea05938d01234">
+  <arcgis-search position="top-right" />
+  <arcgis-legend position="bottom-right" expandable />
+  <arcgis-layer-list position="bottom-left" expandable />
+  <arcgis-feature-layer
+    url="https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Building_Permits/FeatureServer/0"
+    renderer="heatmap"
+    opacity="0.75"
+  />
+</arcgis-map>
+
+<style>
+  arcgis-map {
+    height: 100vh;
+    width: 100vw;
+  }
+</style>
---
+++ src/routes/+server.js
@@ -0,0 +1,4 @@
+import config from "@arcgis/core/config";
+
+config.request.useIdentity = false;
+
